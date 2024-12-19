4a83df15c72baf3018c664efd6a022aba2695d47
Josh <jsbusiness305@gmail.com>
2024-12-19 11:10:46 -0500

    added DigitalClock component

---
+++ src/components/DigitalClock.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from 'react'
+
+function DigitalClock() {
+    return (
+        <div>
+            <h1> Digital Clock</h1>
+        </div>
+    )
+}
+
+export default DigitalClock;
