33298702e455c7659aa5f01dd8bf3d60014d1b05
Josh <jsbusiness305@gmail.com>
2024-12-19 11:42:46 -0500

    added time logic

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,10 +1,31 @@
 import { useState, useEffect } from 'react'
 
 function DigitalClock() {
+
+
+    const [time, setTime] = useState(new Date());
+
+    // only run once on mount
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+        setTime(new Date())
+
+        //clean up
+        return () => clearInterval(interval)
+    }, [])
+
+    const getTime = (time) => {
+        const pmAm = time.getHours() >= 12 ? 'PM' : 'AM'
+        return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} ${pmAm}`
+    }
+
+
     return (
         <div className="clock-container">
             <div className="clock">
-                <span>0:00:00</span> 
+                <span>{getTime(time)}</span> 
                 
             </div>
         </div>
